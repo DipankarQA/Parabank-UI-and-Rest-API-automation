f6520244192b1e102d331e754c8245c60eb22baf
DipankarQA <12014dipsnh@gmail.com>
2025-12-29 09:08:03 +0530

    add

--- pages/Fundtransferpage.py
+++ pages/Fundtransferpage.py
@@ -7,20 +7,29 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 class Fundtransferpage():
     def __init__(self, page):
         self.page = page
-        self.f=NewAccountOpenningPage(page)
-        toacc =self.f.acc_creation(page)
+        #self.f=NewAccountOpenningPage(page)
+        #toacc =self.f.acc_creation(page)
     def Fundtransfer(self, amount, fromacc, toacc ):
         self.page.locator("//*[text()='Transfer Funds']").click()
-        self.page.locator("//*[@id='amount']").fill(amount)
-        self.page.locator("//*[@id='fromAccountId']").select_option(value="12345")
-        self.page.locator("//*[@id='toAccountId']").select_option(value = toacc)
+        self.page.locator("//*[@id='amount']").fill(str(amount))
+        """fromacc="12345"
+        self.page.locator("//*[@id='fromAccountId']").select_option(value=fromacc)
+        self.page.locator("//*[@id='toAccountId']").select_option(value = toacc)"""
+        self.page.wait_for_selector("#fromAccountId option")
+        self.page.locator("#fromAccountId").select_option(index=0)
+
+        self.page.wait_for_selector("#toAccountId option")
+        self.page.locator("#toAccountId").select_option(value=str(toacc))
+
         self.page.locator("//*[@value='Transfer']").click()
-        print(self.page.locator("//*[text()='Transfer Complete!']").text_content())
+        successtext=(self.page.locator("//*[text()='Transfer Complete!']").text_content())
+        print(successtext)
         head ={"Accept": "application/xml"}
-        response = requests.get(f"https://parabank.parasoft.com/parabank/services/bank/accounts/{toacc}", headers = head  )
+        response = requests.get(f"https://parabank.parasoft.com/parabank/services/bank/accounts/{toacc}",
+                                 headers = head  )
         print(response.status_code)
         assert 200 == response.status_code
         print("response text:",response.text)
         #assert toacc in response.text
-        return toacc['balance']
+        return toacc
 
